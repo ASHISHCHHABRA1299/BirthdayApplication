8dd30fee635adc830844674479b0c0032a0573e0
hp <chhabraashish123@gmail.com>
2021-05-11 16:56:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/birthdayapplication/birthday.java
@@ -0,0 +1,18 @@
+package com.example.birthdayapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class birthday extends AppCompatActivity {
+
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birthday);
+        mTextView=(TextView)findViewById(R.id.text);
+    }
+}
